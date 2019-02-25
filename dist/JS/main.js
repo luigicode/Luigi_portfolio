ad234678158a7cad7e1dced9440970320922c751
luigi <antoinelisberto@gmail.com>
2019-02-25 10:48:05 +0800

    portrait fixed

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -1,14 +1,15 @@
-// select DOM items
+// Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll("nav-Item");
+const navItems = document.querySelectorAll(".nav-item");
 
-// set initial state of menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
@@ -16,7 +17,8 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
-    //set menu state
+
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
@@ -24,7 +26,8 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
-    //menu state
+
+    // Set Menu State
     showMenu = false;
   }
 }
